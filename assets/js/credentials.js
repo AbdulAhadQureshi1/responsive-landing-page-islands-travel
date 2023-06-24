82740f4a5cdac63585e99798c479d20075e3f8c8
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2023-06-24 19:32:53 +0500

    Add EmailJs

---
+++ assets/js/credentials.js
@@ -0,0 +1,5 @@
+const credentials = {
+    SERVICE_ID : "service_7o6z9y5",
+    PUBLIC_KEY : "NXRD5puTsuUJUp5uI",
+    TEMPLATE_ID : "template_zryuvd5"
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,82 @@ document.onreadystatechange = function () {
   }
 };
 
+/*==================== FORM ====================*/
+const form = document.querySelector("#submit-btn");
+const inputs = document.querySelectorAll(".input");
+const modal = document.querySelector("#modal");
+const close = document.querySelector("#close");
+
+emailjs.init(credentials.PUBLIC_KEY);
+
+form.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  form.innerHTML = "Sending...";
+
+  // DATA VALIDATION
+  const [name, email, sub, msg] = inputs;
+  if (
+    name.value.length === 0 ||
+    email.value.length === 0 ||
+    sub.value.length === 0 ||
+    msg.value.length === 0
+  ) {
+    showDialog("Fill all the fields first!", true);
+    form.innerHTML = "Send Message";
+    return;
+  }
+  const userInputs = {
+    name: inputs[0].value,
+    email: inputs[1].value,
+    subject: inputs[2].value,
+    message: inputs[3].value,
+  };
+  inputs.forEach((input) => {
+    input.value = "";
+  });
+
+  // GET THE VALUES AND RESET THE FORM
+
+  // SEND EMAIL
+
+  const templateParams = {
+    name: userInputs.name,
+    email: userInputs.email,
+    subject: userInputs.subject,
+    body: userInputs.message,
+  };
+
+  emailjs.send(credentials.SERVICE_ID, credentials.TEMPLATE_ID, templateParams).then(
+    function (response) {
+      form.innerHTML = "Send Message";
+      showDialog("Message sent sucessfully", false);
+    },
+    function (error) {
+      form.innerHTML = "Send Message";
+      showDialog("An error has occured", false);
+    }
+  );
+
+});
+
+function showDialog(msg, isError) {
+  modal.showModal();
+  modal.classList.add('active')
+  document.body.style.overflowY = 'hidden';
+  document.querySelector("#msg").innerHTML = (isError
+    ? "<i class='bx bxs-error' style='color:#ff2a00'  ></i>"
+    : "<i class='bx bxs-check-circle' style='color:#2fc305'  ></i>") + msg;
+    close.innerHTML = isError ? 'Try Again' : 'Okay';
+    close.style.backgroundColor = isError ? "#ff2a00" : "#2fc305";
+    close.addEventListener("click", () => {
+      modal.close();
+      document.body.style.overflowY = 'auto';
+      modal.classList.remove('active')
+  });
+}
+
+
 /*==================== SHOW MENU ====================*/
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
@@ -18,10 +94,14 @@ const showMenu = (toggleId, navId) => {
 
   // Validate that variables exist
   if (toggle && nav) {
-    toggle.addEventListener("click", () => {
-      // We add the show-menu class to the div tag with the nav__menu class
-      nav.classList.toggle("show-menu");
-    }, {passive: true});
+    toggle.addEventListener(
+      "click",
+      () => {
+        // We add the show-menu class to the div tag with the nav__menu class
+        nav.classList.toggle("show-menu");
+      },
+      { passive: true }
+    );
   }
 };
 
@@ -94,4 +174,6 @@ function scrollAnimation() {
   islandsPopup.classList.remove("show-popup");
 }
 
-controlImg.forEach((c) => c.addEventListener("click", scrollAnimation, {passive: true}));
+controlImg.forEach((c) =>
+  c.addEventListener("click", scrollAnimation, { passive: true })
+);
