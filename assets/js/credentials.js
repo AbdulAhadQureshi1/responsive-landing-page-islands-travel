8c4f24375ad32ee98b0586c4b8285ff390f8a0df
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2023-08-28 12:25:00 +0500

    fix emailJS

--- assets/js/credentials.js
+++ assets/js/credentials.js
@@ -3,3 +3,8 @@ const credentials = {
     TEMPLATE_ID : "template_d78hnjf",
     PUBLIC_KEY : "IoRXoD_PrftQZAJ9b"
 }
+// const credentials = {
+//     SERVICE_ID : "service_tn0y7ps",
+//     TEMPLATE_ID : "template_y57u2pq",
+//     PUBLIC_KEY : "NXRD5puTsuUJUp5uI"
+// }
